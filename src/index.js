341e4eaa9cf707483e60ac819b4f0a97e5b12760
Misung Kim <misung178@gmail.com>
2020-08-30 17:26:28 +0900

    css setting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.querySelector("body").innerHTML = `
-<h1>GeM</h1>
-`;
+<h1>환경인간</h1>
+`;  
+const browseBtn = document.querySelector('.browse-btn');
+const realInput = document.querySelector('#real-input');
+
+browseBtn.addEventListener('click',()=>{
+	realInput.click();
+});
